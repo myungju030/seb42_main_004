a40eb76bf3ab3101c51feda363d3a5d4244de97f
myungji <myungju030@gmail.com>
2023-05-29 23:44:48 +0900

    [FE_refactor] 자동 로그아웃 기능GlobalLayout으로 이동 및 alert 기능 제거

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,6 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setProfile } from './reducers/userReducer';
-import { setAuth } from './reducers/authReducer';
-import getData from './util/getData';
-import parseToken from './util/parseToken';
-import useInitialize from './util/useInitialize';
 import GlobalLayout from './routes/GlobalLayout';
 
 function App() {
-  const dispatch = useDispatch();
-  const initialize = useInitialize();
-  const accessToken = localStorage.getItem('accessToken');
-
-  useEffect(() => {
-    let logoutTimer;
-    if (accessToken) {
-      const { exp, roles } = parseToken(accessToken);
-      dispatch(
-        setAuth({
-          isLogin: true,
-          admin: roles.includes('ADMIN'),
-        })
-      );
-      getData('/users').then((data) => {
-        dispatch(
-          setProfile({ imagePath: data.imagePath, name: data.name, email: '' })
-        );
-      });
-      const remainingTime = Math.floor(
-        (new Date(exp * 1000).getTime() - new Date().getTime()) / (60 * 1000)
-      );
-      logoutTimer = setTimeout(() => {
-        initialize().then(() => {
-          alert('자동 로그아웃되었습니다.');
-        });
-      }, remainingTime * 60 * 1000);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [accessToken]);
-
   return <GlobalLayout />;
 }
 
--- client/src/routes/GlobalLayout.jsx
+++ client/src/routes/GlobalLayout.jsx
@@ -1,18 +1,52 @@
 import { Suspense, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Loading from '../components/commons/Loading';
-import GlobalStyle from '../global/globalstyles';
-import { useSelector } from 'react-redux';
-import Header from '../components/commons/Header';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import checkFooter from '../util/checkFooter';
+import Loading from '../components/commons/Loading';
+import Header from '../components/commons/Header';
 import ToTopButton from '../components/commons/ToTopButton';
 import Footer from '../components/commons/Footer';
+import GlobalStyle from '../global/globalstyles';
+import { setAuth } from '../reducers/authReducer';
+import { setProfile } from '../reducers/userReducer';
+import checkFooter from '../util/checkFooter';
+import useInitialize from '../util/useInitialize';
+import parseToken from '../util/parseToken';
+import getData from '../util/getData';
 import Router from './Router';
 
 export default function GlobalLayout() {
   const { admin } = useSelector((state) => state.authReducer);
   const { pathname, search } = useLocation();
+  const dispatch = useDispatch();
+  const initialize = useInitialize();
+  const accessToken = localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    let logoutTimer;
+    if (accessToken) {
+      const { exp, roles } = parseToken(accessToken);
+      dispatch(
+        setAuth({
+          isLogin: true,
+          admin: roles.includes('ADMIN'),
+        })
+      );
+      getData('/users').then((data) => {
+        dispatch(
+          setProfile({ imagePath: data.imagePath, name: data.name, email: '' })
+        );
+      });
+      const remainingTime = Math.floor(
+        (new Date(exp * 1000).getTime() - new Date().getTime()) / (60 * 1000)
+      );
+      logoutTimer = setTimeout(() => {
+        initialize();
+      }, remainingTime * 60 * 1000);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [accessToken]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
